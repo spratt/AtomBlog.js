11590e45ee2b087e0ae9c6e1adef6ad8145840f2
Simon Pratt <spratt@scs.carleton.ca>
2013-04-22 18:10:27 -0400

    Added polyfill for String.endsWith

--- AtomBlog.js
+++ AtomBlog.js
@@ -1,4 +1,19 @@
 $(function() {
+	// String.endsWith polyfill from MDN
+	// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String/endsWith
+	if (!String.prototype.endsWith) {
+		Object.defineProperty(String.prototype, 'endsWith', {
+			enumerable: false,
+			configurable: false,
+			writable: false,
+			value: function (searchString, position) {
+				position = position || this.length;
+				position = position - searchString.length;
+				return this.lastIndexOf(searchString) === position;
+			}
+		});
+	}
+	//////////////////////////////////////////////////////////////////////
 	var blogTitle = '';
 	var entries = null;
 	var index = -1;
